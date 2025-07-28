ef92988dce1c06ec6ce1c439856da944de9959ca
kimyounhoex1 <dordyd11@gmail.com>
2025-07-29 01:57:58 +0900

    10869 사칙연산

---
+++ 10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  int num1;
+  int num2;
+  cin >> num1 >> num2;
+
+  cout << num1+num2 << '\n';
+  cout << num1-num2 << '\n';
+  cout << num1*num2 << '\n';
+  cout << num1/num2 << '\n';
+  cout << num1%num2 << '\n';
+  
+  return 0;
+}
