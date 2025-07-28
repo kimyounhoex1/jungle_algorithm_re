6913481ea1df9a0ddfdb923f5d95ed53135d491f
kimyounhoex1 <dordyd11@gmail.com>
2025-07-29 03:15:41 +0900

    9498 시험 성적

--- 2588.cpp
+++ 2588.cpp
@@ -20,7 +20,7 @@ int main(){
   cin >> num1 >> num2;
   int num2_size = size(num2);
   int total = 0;
-  cout << "num2_size = " << num2_size << '\n';
+  // cout << "num2_size = " << num2_size << '\n';
   for(int i = 0; i < num2_size; i++){
     
     int remain = num2%10;
@@ -28,7 +28,7 @@ int main(){
     int temp = num1 * remain;
     cout << temp << '\n';
     total += temp*pow(10, i);
-    cout << "total = " << total << '\n';
+    // cout << "total = " << total << '\n';
   }
 
   cout << total;
